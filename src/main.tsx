f2cbdea941c25c789341beca51fe0cfe9d3068e6
Your Name <you@example.com>
2026-03-22 00:58:02 -0700

    Force cache bust for JS bundle

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log("Global Fishers Initialized v1.0.1");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
